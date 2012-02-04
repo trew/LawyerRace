40ed6c2266a7ffe7942d222a99216a70b8ff90d1
Samuel Andersson <samuelandersson@mensa.se>
2012-02-04 14:21:48 +0100

    Restructured visual studio project

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 //#include "App.h"
 //#include "init_static.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
 //	srand(static_cast<unsigned int>(time(NULL)));
 
 //	App* newApp = new App;
