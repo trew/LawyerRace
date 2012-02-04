37494fe451f0a56fd28cc5ec6b16f91fa84560e1
Samuel Andersson <smulster@gmail.com>
2012-02-04 11:43:17 +0100

    Created VS2010 project and configured appropriate ignores for Git.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+*  
+*
+*/
+
+//#include <ctime>
+//#include <SDL.h>
+//#include "App.h"
+//#include "init_static.h"
+
+int main(int argc, char** argv) {
+//	srand(static_cast<unsigned int>(time(NULL)));
+
+//	App* newApp = new App;
+//	return newApp->Execute();
+	return 0;
+}
