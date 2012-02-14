32450971a4270e1fca2287624fc42e8ca0b64f95
Samuel Andersson <smulster@gmail.com>
2012-02-14 22:21:10 +0100

    added try-catch in main-function

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,22 @@
 #include <SDL.h>
 #include <ctime>
 #include "Game.h"
+#include <iostream>
+
 //#include "init_static.h"
 
 int main(int argc, char* argv[]) {
 	srand(static_cast<unsigned int>(time(NULL)));
 
-	Game* newGame = new Game;
-	return newGame->Execute();
+	Game newGame;
+    try 
+    {
+        newGame.Execute();
+    } 
+    catch 
+    {
+        std::cerr << "Something went terribly wrong." << std::endl;
+    }
+	return 0;
 }
+
